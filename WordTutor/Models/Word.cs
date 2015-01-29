86f5149de193a97cddded4788c7803871e891520
Bakhshi Mnatsakanyan <bmnatsakanyan@macadamian.com>
2015-01-29 10:30:53 +0400

    Learning and Learned word lists
    - use MVVM pattern to show content on the Learned and Learning pages
    - use hardcoded data for the words, will be removed after adding support
      of reading them from the file

---
+++ WordTutor/Models/Word.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WordTutor.Models
+{
+    public class Word : INotifyPropertyChanged
+    {
+        private string _text;
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    NotifyPropertyChanged("Text");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
