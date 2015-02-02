f651c5b702ba62b3c165445f9faefa439c0081f1
Bakhshi Mnatsakanyan <bmnatsakanyan@macadamian.com>
2015-02-02 15:54:23 +0400

    Use json to store data
    - load words from json file
    - save wards to json file on application suspension
    
    NOTE:
      Use Visual Studios' Lifecycle Events to send suspension, shutdown requests, cause
      they do not fired correctly in the debug mode.

---
+++ WordTutor/WordTutor/Models/Description.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordTutor.Models
+{
+    [DataContract]
+    public class Description : INotifyPropertyChanged
+    {
+        private string _text;
+
+        [DataMember(Name = "text")]
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    NotifyPropertyChanged("Text");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
